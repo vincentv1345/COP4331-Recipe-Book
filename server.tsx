5e1b081f7fe03b5d29fb2a7f19fa22786402ea7f
Jesse Johnson <jessejohnson1423@gmail.com>
2022-10-26 18:44:25 -0400

    Initial commit of login function

--- server.tsx
+++ server.tsx
@@ -49,7 +49,36 @@ app.get('/', function (req, res) {
             });
     });
 });
- 
+
+// login wiht username and password
+app.post('/api/login', async (req, res, next) => 
+{
+  // incoming: login, password
+  // outgoing: id, firstName, lastName, error
+	
+ var error = '';
+
+  const { login, password } = req.body;
+
+  const db = client.db("COOKBOOKDATABASE"); 
+  const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
+
+  var id = -1;
+  var fn = '';
+  var ln = '';
+
+  if( results.length > 0 )
+  {
+    id = results[0].UserID;
+    fn = results[0].FirstName;
+    ln = results[0].LastName;
+  }
+
+  var ret = { id:id, firstName:fn, lastName:ln, error:''};
+  res.status(200).json(ret);
+});
+
+
 var server = app.listen(5000, function () {
     console.log('Server is listening at port 5000...');
 });
