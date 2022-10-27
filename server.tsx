8f2e53a5b295460d50b249e2479407a7a9a5ec2b
Jesse Johnson <jessejohnson1423@gmail.com>
2022-10-27 10:21:30 -0400

    Major clean up and package downloads

--- server.tsx
+++ server.tsx
@@ -1,3 +1,4 @@
+/*
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 const url = process.env.MONGODB_URI;
@@ -18,7 +19,21 @@ app.use(cors());
 app.use(bodyParser.json());
 const mssql = require("mysql");
  
+*/
+
+const express = require('express'); // Used to create API
+const bodyParser = require('body-parser'); // Checks HTML and JSON parsing
+const cors = require('cors'); // Prevents CORS errors
+
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.set( 'port', (process.env.PORT || 5000 ));
+app.use(cors());
+app.use(bodyParser.json());
+
 // Get request
+/*
 app.get('/', function (req, res) {
  
     // Config your database credential
@@ -49,14 +64,15 @@ app.get('/', function (req, res) {
             });
     });
 });
+*/
 
-// login wiht username and password
+// login with username and password
 app.post('/api/login', async (req, res, next) => 
 {
   // incoming: login, password
   // outgoing: id, firstName, lastName, error
 	
- var error = '';
+  var error = '';
 
   const { login, password } = req.body;
 
@@ -73,12 +89,54 @@ app.post('/api/login', async (req, res, next) =>
     fn = results[0].FirstName;
     ln = results[0].LastName;
   }
+  /*
+  if(login.toLowerCase() == "jesse" && password == "j")
+  {
+      id = 1;
+      fn = 'Jesse';
+      ln = 'Johnson';
+  }
+  else
+  {
+      error = "login error";
+  }
+  */
 
   var ret = { id:id, firstName:fn, lastName:ln, error:''};
   res.status(200).json(ret);
 });
 
+// Handles all incoming requests we havnt handled from above
+app.use((req, res, next) => 
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
 
+
+/*
 var server = app.listen(5000, function () {
     console.log('Server is listening at port 5000...');
+});
+*/
+
+// For testing, DELETE
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+//Gets dynamically given port number from Heroku
+
+app.listen(PORT, () => 
+{
+  console.log('Server listening on port ' + PORT);
 });
