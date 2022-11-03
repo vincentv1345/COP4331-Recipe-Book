be1fe6850f1a6628545881b70fb6419d52a6a58c
taylora-c <tayloralarconcometta@knights.ucf.edu>
2022-11-03 16:46:19 -0400

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
