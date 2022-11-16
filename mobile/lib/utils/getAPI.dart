50eabd659c4d30427d4880d10a5d2d9864dcfc87
Joshua Easterling <joshua.easterling3@icloud.com>
2022-11-16 14:39:18 -0500

    small changes to getAPI.dart

--- mobile/lib/utils/getAPI.dart
+++ mobile/lib/utils/getAPI.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class RecipeData {
   //base url for api can access specific API functions with base + '/function/
-  var base = 'https://recipebook5959.herokuapp.com/api/';
+
 
   static Future<String> getJson(String url, String outgoing) async
   {
@@ -31,20 +31,16 @@ class RecipeData {
   }
 
   //complete API request for login
-  Future<String> doLogin(String base, String email, String password,String outgoing) async
+  Future<String> doLogin(String url, String email, String password) async
   {
-    var url = base + '/login';
     var ret = "";
+
+    var jsonBody = {"email": "email", "password":"password"};
+
     try
     {
       http.Response response = await http.post(url,
-          body: utf8.encode(outgoing),
-          headers:
-          {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-          },
-          encoding: Encoding.getByName("utf-8")
+        body: jsonBody,
       );
       ret = response.body;
     }
@@ -56,5 +52,30 @@ class RecipeData {
     return ret;
 
   }
+  /*
+  Future<String> CreateAccount(String url, String email, String password) async
+  {
+
+    var ret = "";
+
+    //var jsonBody = {"email": "email", "password":"password"};
+
+    try
+    {
+      http.Response response = await http.post(url,
+        body: jsonBody,
+      );
+      ret = response.body;
+    }
+    catch (e)
+    {
+      print(e.toString());
+    }
+
+    return ret;
+
+  }
+
+   */
 
 }
